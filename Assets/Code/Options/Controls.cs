969a5120feca6d732619e11b74269fa8f583825e
unknown <hndrkm>
2023-01-16 11:00:34 -0400

    guardando el juego de aiko

---
+++ Assets/Code/Options/Controls.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AikoGame 
+{
+    [System.Serializable]
+    public class Controls
+    {
+        public KeyCode forwards, backward, strafeRight, strafeLeft, rotateRight, walkRun;
+    }
+
+}
---
+++ Assets/Code/Player/PlayerController.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace AikoGame 
+{
+    public class PlayerController : MonoBehaviour
+    {
+        public Controls _controls;
+        void Start()
+        {
+
+        }
+
+        
+        void Update()
+        {
+
+        }
+    }
+
+
+}
